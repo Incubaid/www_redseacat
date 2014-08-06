36258e5c9b93c9b787d4f2880e3bcf6d84cd91ec
mtayseer <tayseerm@codescalers.com>
2014-08-06 11:02:49 +0200

    Removed unused template to avoid confusion

--- .files/js/theme.js
+++ .files/js/theme.js
@@ -73,3 +73,10 @@ var servicesCircle = {
     }
 }
 
+
+    $(document).ready(function(){
+    var str=location.href.toLowerCase();
+        $('li.active').removeClass("active");
+    $('.nav li a').filter(function() {return this.href.toLowerCase() == str; }).parents('li').addClass('active');
+         
+})
